500c552b864ca74fc4040c7fe8b4f50a1ce9ea6e
Fernando Flores <fernando.flores112015@gmail.com>
2025-04-01 16:43:44 -0600

    add env

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -2,9 +2,10 @@
 
 
 export const getGifs = async (category) => {
-  const API_KEY='HOJg3p0pCQHxbMl5xvvNv6eGWG79o5RC'
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${category}&limit=8`;
+    const API_KEY=`${import.meta.env.VITE_GIPHY_API_KEY}`
+    const url = `${import.meta.env.VITE_VASE_URL}?api_key=${API_KEY}&q=${category}&limit=8`;
    
+
     const resp = await fetch(url);
     const { data } = await resp.json();
 
@@ -14,6 +15,5 @@ export const getGifs = async (category) => {
         url: img.images.downsized_medium.url,
     }));
 
-    //console.log(gifs);
     return gifs;
 }
